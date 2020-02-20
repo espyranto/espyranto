d423aa85345ceaacf75310629e0919ca377641d1
John Kitchin <johnrkitchin@gmail.com>
2020-02-20 13:11:27 -0500

    change import to remove warning

--- espyranto/g1/mmolH.py
+++ espyranto/g1/mmolH.py
@@ -317,7 +317,7 @@ class mmolH:
 
     def slideshow(self, cache=True):
         from IPython.display import Image, display
-        from IPython.html.widgets import interact
+        from IPython.notebook.widgets import interact
 
         if cache == False:
             images = [Image(open(f,'rb').read(), width=800)
